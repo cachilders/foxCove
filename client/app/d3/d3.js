8b735544e156f309f0f619d1c79f5d0000d382fb
Eric Churchill <eric.churchill2@gmail.com>
2016-09-07 20:44:38 -0700

    [bug out] Fix bug in D3 graphic

--- client/app/d3/d3.js
+++ client/app/d3/d3.js
@@ -16,11 +16,6 @@ angular.module('d3directive', [])
         var color = d3.scaleOrdinal()
           .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-        var pie = d3.pie()
-          .sort(null)
-          .value(function(d) {
-            return d.rate;
-          });
         var svg = d3.select(element[0])
           .append('svg')
           .style('width', '100%')
@@ -54,7 +49,7 @@ angular.module('d3directive', [])
           .value(function(d) {
             return d.rate;
           })
-          .startAngle();
+          // .startAngle();
 
         function render(data){
 
